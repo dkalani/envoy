0b83b357cc8b494909d03473afdc659e78ccb47d
Jose Nino <jnino@lyft.com>
2018-09-14 17:23:14 -0700

    http2: more error callback
    Signed-off-by: Jose Nino <jnino@lyft.com>

--- source/common/http/http2/codec_impl.cc
+++ source/common/http/http2/codec_impl.cc
@@ -729,6 +729,7 @@ ConnectionImpl::Http2Callbacks::Http2Callbacks() {
         return static_cast<ConnectionImpl*>(user_data)->onInvalidFrame(frame->hd.stream_id,
                                                                        error_code);
       });
+      
   nghttp2_session_callbacks_set_error_callback(
       callbacks_,
       [](nghttp2_session*, const nghttp2_frame* frame, int error_code, void* user_data) -> int {
